17f879b6d5d409e686f324f01e02982546ac84c2
Hanif Luqmanul Hakim <hanifluqmanulhakim20@gmail.com>
2025-05-16 10:22:25 +0700

    Create segitiga-bintang-kreatif.cpp

---
+++ segitiga-bintang-kreatif.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i, j, baris;
+
+    cout << "Segitiga Bintang Siku-Siku Terbalik\n";
+    cout << "Masukkan jumlah baris : ";
+    cin >> baris;
+
+    for (i = baris; i >= 1; i--) {
+        for (j = 1; j <= i; j++) {
+            cout << ":v ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
